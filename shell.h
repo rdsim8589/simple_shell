b4f494487c3e82ff1409b3ac1fb97ef86cc041a4
Richard Sim <rdsim8589@gmail.com>
2016-12-14 22:21:03 +0000

    updated header with labels and changes prototypes to take in arugement hist_head

--- shell.h
+++ shell.h
@@ -17,16 +17,39 @@ extern char **environ;
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-
+/**
+ * struct env_s - a singly linked list
+ * @name:
+ * @value:
+ * @next:
+ *
+ * Description:
+ */
 typedef struct env_s
 {
 	char *name;
 	char *value;
 	struct env_s *next;
 } env_t;
+/**
+ * struct hist_s - a singly linked list
+ * @name:
+ * @value:
+ * @next:
+ *
+ * Description:
+ */
+typedef struct hist_s
+{
+	char *entry;
+	struct hist_s *next;
+} hist_t;
 
+/* mem.c prototypes */
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *get_line(int file);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/* getline.c prototype */
+char *get_line(int file, hist_t **hist_head);
 char **buildEnv(env_t *head, int *envsize);
 env_t *addEnv(env_t **head, char *name, char *value);
 void free_list(env_t *head);
@@ -45,8 +68,7 @@ char *dir_concat(char *s1, char *s2);
 void addHistory(char **history, char *command);
 char *trim(char *string, int count);
 char *_strdup(char *str);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int checkBuiltins(char *tok, char *save, env_t **head);
+int checkBuiltins(char *inp, char *save, env_t **environ, hist_t **hist_head);
 int _strcmp(char *s1, char *s2);
 env_t *getEnvPtr(char *name, env_t *head);
 char **getArgs(char *tok, char *argv[], char *save);
@@ -55,4 +77,8 @@ int cdBuiltin(char *save, env_t *head);
 env_t *setEnvPtr(char *envname, char *value, env_t *head);
 char *str_concat(char *s1, char *s2);
 int _atoi(char *s);
+/* history.c prototypes*/
+hist_t *add_hist(int total, hist_t **hist_head, char *buf);
+void clear_hist(hist_t **hist_head);
+void print_hist(hist_t *hist_head);
 #endif
